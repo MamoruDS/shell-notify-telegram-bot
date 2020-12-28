706b1135fdd9d88ec2569d9b93aa55515fd06a03
MamoruDS <mamoruds.io@gmail.com>
2020-12-28 22:49:53 +0900

    added: new build script

--- build.sh
+++ build.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
 
-_PLAT=$1
-_ARCH=$2
+_VERSION=$(jq -r ".version" package.json)
 
 rm -rf dist ; tsc
+
 # platform freebsd, linux, alpine, macos, win
 # arch x64, x86, armv6, armv7
-pkg dist/main.js --target node14-$_PLAT-$_ARCH --output notify.temp
 
-cp notify.temp notify
+# linux_64
+pkg dist/main.js --target node14-linux-x64 --output notify \
+    && tar zvcf release/notify_${_VERSION}_linux_x64.tar.gz notify > /dev/null \
+    && rm notify \
+    && echo "> build complete: notify_${_VERSION}_linux_x64.tar.gz"
+
+# macos_64
+pkg dist/main.js --target node14-macos-x64 --output notify \
+    && tar zvcf release/notify_${_VERSION}_macos_x64.tar.gz notify > /dev/null \
+    && rm notify \
+    && echo "> build complete: notify_${_VERSION}_macos_x64.tar.gz"
+
+# win_64
+pkg dist/main.js --target node14-win-x64 --output notify.exe \
+    && zip release/notify_${_VERSION}_win_x64.zip notify.exe > /dev/null \
+    && rm notify.exe \
+    && echo "> build complete: notify_${_VERSION}_win_x64.zip"
