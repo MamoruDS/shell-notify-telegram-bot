cf84be914b906ca6199b159379187615be86b31b
MamoruDS <mamoruds.io@gmail.com>
2021-01-02 12:58:24 +0900

    updated: examples/progress.ts

--- examples/progress.ts
+++ examples/progress.ts
@@ -8,7 +8,9 @@ const encoder = new TextEncoder()
 
 const print = (cur: number): void => {
     const _prog = Math.round((cur / TARGET) * BAR_LEN)
-    const bar = `\r[${'■'.repeat(_prog)}${'-'.repeat(BAR_LEN - _prog)}]`
+    let _bl = BAR_LEN - _prog
+    _bl = _bl < 0 ? 0 : _bl
+    const bar = `\r[${'■'.repeat(_prog)}${'-'.repeat(_bl)}]`
     Deno.stdout.write(encoder.encode(`${bar} ${cur.toFixed(2)}/${TARGET}`))
 }
 
@@ -27,4 +29,4 @@ const next = (cur: number, step: number) => {
 
 console.log('some progress:')
 
-next(0, 2)
+next(0, 5)
