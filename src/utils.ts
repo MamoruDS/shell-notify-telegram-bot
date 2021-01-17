70a0b558e25440034bc4a1171ec898668c36f4c2
MamoruDS <mamoruds.io@gmail.com>
2021-01-17 18:02:12 +0900

    added: methods copy and compObjCopy for object deep copy in src/utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -63,4 +63,48 @@ const humanizeDuration = (duration: number): [string, string] => {
     return t
 }
 
-export { randStr, safeMDv2, safeTag, wait, panic, humanizeDuration }
+const copy = <T>(source: T): T => {
+    if (source == null) return source
+    if (Array.isArray(source)) {
+        const _t = [] as any[]
+        source.forEach((v) => {
+            _t.push(copy(v))
+        })
+        return _t as any
+    }
+    if (typeof source === 'object') {
+        if (source.constructor.name !== 'Object') {
+            return source
+        }
+        const _t = {} as T
+        for (const key of Object.keys(source)) {
+            _t[key] = copy(source[key])
+        }
+        return _t
+    }
+    return source
+}
+
+const compObjCopy = <T extends object>(source: object): T => {
+    if (typeof source != 'object' || source === null || Array.isArray(source)) {
+        throw new TypeError()
+    }
+    if (source == {}) {
+        return {} as T
+    }
+    const _t = {} as T
+    for (const key of Object.keys(source)) {
+        _t[key] = copy(source[key])
+    }
+    return _t
+}
+
+export {
+    randStr,
+    safeMDv2,
+    safeTag,
+    wait,
+    panic,
+    humanizeDuration,
+    compObjCopy,
+}
